feb3c7e372e6b8cf2dbeeaf598638495b28cb99f
uutkuutar <114922033+uutkuutar@users.noreply.github.com>
2022-10-11 15:40:34 +0300

    Vize-Final Hesaplama

---
+++ vizefinal/vizefinal/vizefinal/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace vizefinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numvize = new System.Windows.Forms.NumericUpDown();
+            this.numfinal = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numvize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numfinal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vize Notu :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(8, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Final Notu :";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(159, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(8, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "SONUÇ :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(108, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(104, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Not";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(255, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Harf Notu";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(424, 219);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Durumu";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(254, 244);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "label8";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(423, 244);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 20);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "label9";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // numvize
+            // 
+            this.numvize.DecimalPlaces = 1;
+            this.numvize.Location = new System.Drawing.Point(159, 36);
+            this.numvize.Name = "numvize";
+            this.numvize.Size = new System.Drawing.Size(120, 20);
+            this.numvize.TabIndex = 12;
+            // 
+            // numfinal
+            // 
+            this.numfinal.DecimalPlaces = 1;
+            this.numfinal.Location = new System.Drawing.Point(159, 72);
+            this.numfinal.Name = "numfinal";
+            this.numfinal.Size = new System.Drawing.Size(120, 20);
+            this.numfinal.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(410, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 56);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Yenile";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(546, 306);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numfinal);
+            this.Controls.Add(this.numvize);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Not Hesaplama";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numvize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numfinal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numvize;
+        private System.Windows.Forms.NumericUpDown numfinal;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ vizefinal/vizefinal/vizefinal/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vizefinal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = "";
+            label8.Text = "";
+            label9.Text = "";
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+            try
+            {
+                double num1, num2;
+                num1 = Convert.ToDouble(numvize.Value);
+                num2 = Convert.ToDouble(numfinal.Value);
+                double numort = num1 * 0.4 + num2 * 0.6;
+                label4.Text = numort.ToString();
+                label8.Text = numort.ToString();
+                label9.Text = numort.ToString();
+
+                if (numort <= 100 && numort >= 92)
+                {
+                    label8.Text = "AA";
+                    label9.Text = "GEÇTİ";
+                    label9.ForeColor = Color.Green;
+                }
+                else if (numort <= 91 && numort >= 85)
+                {
+                    label8.Text = "BA";
+                    label9.Text = "GEÇTİ";
+                    label9.ForeColor = Color.Green;
+                }
+                else if (numort <= 84 && numort >= 78)
+                {
+                    label8.Text = "BB";
+                    label9.Text = "GEÇTİ";
+                    label9.ForeColor = Color.Green;
+                }
+                else if (numort <= 77 && numort >= 71)
+                {
+                    label8.Text = "CB";
+                    label9.Text = "GEÇTİ";
+                    label9.ForeColor = Color.Green;
+                }
+                else if (numort <= 70 && numort >= 64)
+                {
+                    label8.Text = "CC";
+                    label9.Text = "GEÇTİ";
+                    label9.ForeColor = Color.Green;
+                }
+                else if (numort <= 63 && numort >= 57)
+                {
+                    label8.Text = "DC";
+                    label9.Text = "GEÇTİ";
+                    label9.ForeColor = Color.Green;
+                }
+                else if (numort <= 56 && numort >= 50)
+                {
+                    label8.Text = "DD";
+                    label9.Text = "GEÇTİ";
+                    label9.ForeColor = Color.Green;
+                }
+                else if (numort <= 49 && numort >= 40)
+                {
+                    label8.Text = "FD";
+                    label9.Text = "KALDI";
+                    label9.ForeColor = Color.Red;
+                }
+                else if (numort <= 39 && numort >= 1)
+                {
+                    label8.Text = "FF";
+                    label9.Text = "KALDI";
+                    label9.ForeColor = Color.Red;
+                }
+                else if (numort == 0)
+                {
+                    label8.Text = "FF";
+                    label9.Text = "KALDI";
+                    label9.ForeColor = Color.Red;
+                }
+                else
+                {
+                    MessageBox.Show("Değerleri kontrol ediniz!!");
+                }
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message); 
+            }
+
+
+
+        }
+
+        private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            MessageBox.Show("Bu uygulama üniversitelerin genel vize-final oranları (%40-%60) baz alınarak yapılmıştır.", "Yardım");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
